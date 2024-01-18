c11800bbea197fa189b1f176b9e26fae80376414
emclaughlin2 <emlaugh1020@gmail.com>
2024-01-18 14:09:26 -0500

    Add first version of make queue list, will need to be updated to take in arguments from user

---
+++ ihcal/make_queue_list.py
@@ -0,0 +1,21 @@
+#for i in range(27643,27693):
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb16-000{i}-0000.prdf, ihcal_towers_16_{i}.root, hot_tower_analysis_ihcal_16_{i}.root, high_chi2_analysis_ihcal_16_{i}.root, hta_ihcal_16_{i}.txt, hot_tower_waveform_ihcal_16_{i}.root, waveforms_ihcal_16_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb16-000{i}-0000.prdf, ohcal_towers_16_{i}.root, hot_tower_analysis_ohcal_16_{i}.root, high_chi2_analysis_ohcal_16_{i}.root, hta_ohcal_16_{i}.txt, hot_tower_waveform_ohcal_16_{i}.root, waveforms_ohcal_16_{i}.root")
+    
+for i in range(28114,28215):
+#for i in range(27714,27795):
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb16-000{i}-0000.prdf, ihcal_towers_16_{i}.root, hot_tower_analysis_ihcal_16_{i}.root, high_chi2_analysis_ihcal_16_{i}.root, hta_ihcal_16_{i}.txt, hot_tower_waveform_ihcal_16_{i}.root, waveforms_ihcal_16_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb17-000{i}-0000.prdf, ihcal_towers_17_{i}.root, hot_tower_analysis_ihcal_17_{i}.root, high_chi2_analysis_ihcal_17_{i}.root, hta_ihcal_17_{i}.txt, hot_tower_waveform_ihcal_17_{i}.root, waveforms_ihcal_17_{i}.root")
+    
+    print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb16-000{i}-0000.prdf, ohcal_towers_16_{i}.root, hot_tower_analysis_ohcal_16_{i}.root, high_chi2_analysis_ohcal_16_{i}.root, hta_ohcal_16_{i}.txt, hot_tower_waveform_ohcal_16_{i}.root, waveforms_ohcal_16_{i}.root")
+    print(f"/sphenix/lustre01/sphnxpro/commissioning/HCal/junk/pedestal_seb17-000{i}-0000.prdf, ohcal_towers_17_{i}.root, hot_tower_analysis_ohcal_17_{i}.root, high_chi2_analysis_ohcal_17_{i}.root, hta_ohcal_17_{i}.txt, hot_tower_waveform_ohcal_17_{i}.root, waveforms_ohcal_17_{i}.root")
+
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb00-000{i}-0000.prdf, cemc_towers_00_{i}.root, hot_tower_analysis_00_{i}.root, high_chi2_analysis_00_{i}.root, hta_00_{i}.txt, hot_tower_waveform_cemc_00_{i}.root, waveforms_cemc_00_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb01-000{i}-0000.prdf, cemc_towers_01_{i}.root, hot_tower_analysis_01_{i}.root, high_chi2_analysis_01_{i}.root, hta_01_{i}.txt, hot_tower_waveform_cemc_01_{i}.root, waveforms_cemc_01_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb02-000{i}-0000.prdf, cemc_towers_02_{i}.root, hot_tower_analysis_02_{i}.root, high_chi2_analysis_02_{i}.root, hta_02_{i}.txt, hot_tower_waveform_cemc_02_{i}.root, waveforms_cemc_02_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb03-000{i}-0000.prdf, cemc_towers_03_{i}.root, hot_tower_analysis_03_{i}.root, high_chi2_analysis_03_{i}.root, hta_03_{i}.txt, hot_tower_waveform_cemc_03_{i}.root, waveforms_cemc_03_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb04-000{i}-0000.prdf, cemc_towers_04_{i}.root, hot_tower_analysis_04_{i}.root, high_chi2_analysis_04_{i}.root, hta_04_{i}.txt, hot_tower_waveform_cemc_04_{i}.root, waveforms_cemc_04_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb05-000{i}-0000.prdf, cemc_towers_05_{i}.root, hot_tower_analysis_05_{i}.root, high_chi2_analysis_05_{i}.root, hta_05_{i}.txt, hot_tower_waveform_cemc_05_{i}.root, waveforms_cemc_05_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb06-000{i}-0000.prdf, cemc_towers_06_{i}.root, hot_tower_analysis_06_{i}.root, high_chi2_analysis_06_{i}.root, hta_06_{i}.txt, hot_tower_waveform_cemc_06_{i}.root, waveforms_cemc_06_{i}.root")
+    #print(f"/sphenix/lustre01/sphnxpro/commissioning/emcal/junk/pedestal_seb07-000{i}-0000.prdf, cemc_towers_07_{i}.root, hot_tower_analysis_07_{i}.root, high_chi2_analysis_07_{i}.root, hta_07_{i}.txt, hot_tower_waveform_cemc_07_{i}.root, waveforms_cemc_07_{i}.root")
+    
